be8ff036cf67b2f86392fb84d8fe2e4eca9db0ac
김시은 <tldms3241@gmail.com>
2025-04-30 15:02:29 +0900

    feature : scss 스타일 변경 및 적용

--- client/src/features/header/Header.tsx
+++ client/src/features/header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.scss';
 
 const Header = () => {
 	return (
-		<div className="header-wrapper flex-center">
+		<div className="header-wrapper flex-center heading1">
 			<div className="header-item flex-row-center">
 				<img src="/assets/logo.png" className="logo-img" alt="logo" />
 				<div className="title-name">두런두런</div>
