60e75d7add6e383468669ca97a1f7cf8f2d7152b
김시은 <103474525+sieuno3o@users.noreply.github.com>
2025-05-21 10:12:30 +0900

    DL FE #86 - 마이페이지 프로필 구현 (#111)
    * move : 로그아웃 버튼 폴더 변경
    
    * add : 프로필 컴포넌트
    
    * add : 가입한 그룹 리스트 컴포넌트
    
    * style : 스타일 수정
    
    * feat : 컴포넌트 추가
    
    * feat : header 추가
    
    * add : 파일 추가
    
    * feat : 프로필 기능 구현
    
    * feat : myinfo api 수정
    
    * feat : 마이페이지 프로필 구현

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,9 +1,19 @@
 import NavBar from 'components/NavBar';
-import LogoutButton from 'features/logoutButton/LogoutButton';
+import Header from 'features/header/Header';
+import LogoutButton from 'features/myPage/LogoutButton';
+import Profile from 'features/myPage/Profile';
+// import JoinGroupList from 'features/myPage/JoinGroupList';
+import '../features/header/Header.scss';
+import StudyGroupsList from 'features/studyGroupList/StudyGroupsList';
 
 const MyPage = () => {
 	return (
 		<div>
+			<div className="my-page-wrapper">
+				<Header title="마이페이지" showLogo={false} />
+			</div>
+			<Profile />
+			<StudyGroupsList searchResults={null} />
 			<LogoutButton />
 			<NavBar />
 		</div>
