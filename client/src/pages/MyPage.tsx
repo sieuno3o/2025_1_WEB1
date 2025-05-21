ee2190b5712af0bb7b9ae729fdef9f5cf8fb395c
yeeeon02 <yeonn128902@naver.com>
2025-05-21 17:32:04 +0900

    chore: 임시 myGroupIds prop 추가
    TS 오류 방지를 위해 MyPage 컴포넌트에 임시 myGroupIds prop 추가

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -5,15 +5,18 @@ import Profile from 'features/myPage/Profile';
 // import JoinGroupList from 'features/myPage/JoinGroupList';
 import '../features/header/Header.scss';
 import StudyGroupsList from 'features/studyGroupList/StudyGroupsList';
+import { useMyGroupIds } from 'hooks/useMyGroupIds';
 
 const MyPage = () => {
+	const { myGroupIds } = useMyGroupIds();
+
 	return (
 		<div>
 			<div className="my-page-wrapper">
 				<Header title="마이페이지" showLogo={false} />
 			</div>
 			<Profile />
-			<StudyGroupsList searchResults={null} />
+			<StudyGroupsList searchResults={null} myGroupIds={myGroupIds} />
 			<LogoutButton />
 			<NavBar />
 		</div>
